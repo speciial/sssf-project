09a2f147ea68ee28b895970537ac573fb7baf211
Christian <c.christian.heidemann@gmail.com>
2020-04-19 10:25:32 +0300

    Small express app to set up CI with GitHub Actions

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.status(200).send("<h1>Welcome to the Game</h1>");
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
