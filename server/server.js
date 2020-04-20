9b884c777d9533fa5dc25eed58b0a693e7af93f1
ImBluue <baudvalentin74@gmail.com>
2020-04-20 10:53:06 +0200

    add db connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+'use strict'
+require("dotenv").config();
 const express = require('express');
 const app = express();
+const db = require('./database/db');
 
 const PORT = 3000;
 
@@ -7,4 +10,8 @@ app.get('/', (req, res) => {
   res.status(200).send('<h1>Welcome to the Game</h1>');
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
+db.on('connected', () => {
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+});
