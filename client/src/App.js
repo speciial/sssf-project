7078c3c216bcac2936d628c1d713ca129b8c699f
Christian <c.christian.heidemann@gmail.com>
2020-04-22 15:27:30 +0300

    Re-added some client link to fix tests

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,14 @@ class App extends Component {
   render() {
     return (
       <Router>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
         <div>
           <nav>
             <ul>
