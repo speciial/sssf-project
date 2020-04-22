31472dd8f56c07ee6ab12216ece9d50098b901e5
Christian <c.christian.heidemann@gmail.com>
2020-04-22 15:14:10 +0300

    Added basic navigation and material example to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Materials from './components/Materials';
+import Users from './components/Users';
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/materials">Materials</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+                  renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/materials">
+              <Materials />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
---
+++ client/src/components/Users.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  state = {};
+  render() {
+    return <h1>Users</h1>;
+  }
+}
+
+export default Users;
