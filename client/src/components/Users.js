319dadf4d8470c43f801e4438dff9165273b5068
ImBluue <baudvalentin74@gmail.com>
2020-04-24 14:04:58 +0200

    Add button to disconnect

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { isAuth, getAuthUser } from "../utils/Auth";
+import { isAuth, getAuthUser, disconnectUser } from "../utils/Auth";
+import Button from "@material-ui/core/Button";
 
 const Users = () => {
   const history = useHistory();
@@ -11,6 +12,11 @@ const Users = () => {
   } else {
     const user = getAuthUser();
 
+    const disconnect = (e) => {
+      disconnectUser();
+      history.push("/signin");
+    };
+
     return (
       <div>
         <h1>Hello {user.FirstName} !</h1>
@@ -18,6 +24,15 @@ const Users = () => {
         <p>Last name : {user.LastName}</p>
         <p>Username : {user.Username}</p>
         <p>Email : {user.Email}</p>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          onClick={disconnect}
+        >
+          Disconnect
+        </Button>
       </div>
     );
   }
