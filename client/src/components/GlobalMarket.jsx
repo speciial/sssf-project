552a1ba07b07f1b4e66fcea2ecc9db27f7e56959
Christian <c.christian.heidemann@gmail.com>
2020-04-30 16:22:19 +0200

    Added a bunch of assest and _finally_ got client side selling to work

--- client/src/components/GlobalMarket.jsx
+++ client/src/components/GlobalMarket.jsx
@@ -1,22 +1,82 @@
 import React from 'react';
 
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
 import GlobalMarketEntry from './GlobalMarketEntry';
 
-const GlobalMarket = () => {
-  const renderEntries = () => {
-    let entries = [];
-    for (let i = 0; i < 10; i++) {
-      entries.push(<GlobalMarketEntry key={i} />);
+const globalMarketQuery = gql`
+  {
+    marketByName(Name: "global") {
+      id
+      Name
+      Entries {
+        id
+        User {
+          id
+          Username
+        }
+        SuggestedPrice
+        Materials {
+          id
+          Material {
+            id
+            Name
+            Picture
+          }
+          Quantity
+        }
+      }
     }
-    return entries;
-  };
+  }
+`;
+
+const GlobalMarket = () => {
+  const { loading, error, data } = useQuery(globalMarketQuery);
+
+  if (loading) return <p>Loading</p>;
+
+  if (error) console.log(error);
+
+  console.log(data);
+
+  const entries = data.marketByName.Entries;
 
   return (
     <div>
       <h1>Global Market</h1>
-      {renderEntries()}
+      {entries.map((entry) => {
+        return <GlobalMarketEntry key={entry.id} entry={entry} />;
+      })}
     </div>
   );
 };
 
 export default GlobalMarket;
+
+/*
+  "data": {
+    "markets": [
+      {
+        "Name": "global",
+        "Entries": [
+          {
+            "User": {
+              "Username": "test"
+            },
+            "SuggestedPrice": 3000,
+            "Materials": [
+              {
+                "Material": {
+                  "Name": "Trees",
+                  "Picture": "Trees"
+                },
+                "Quantity": 50
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+ */
--- client/src/components/GlobalMarketEntry.jsx
+++ client/src/components/GlobalMarketEntry.jsx
@@ -1,69 +1,77 @@
 import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import {
-  Grid,
-  GridList,
-  Paper,
-  GridListTile,
-  Typography,
-} from '@material-ui/core';
+import { Grid, GridList, Paper, GridListTile } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
+  image: {
+    width: 120,
+    height: 'auto',
+  },
   paper: {
-    padding: theme.spacing(2),
-    textAlign: 'left',
-    backgroundColor: '#fafafa',
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    backgroundColor: '#e0e0e0',
   },
-  gridList: {
-    padding: 0,
-    margin: 0,
-  },
-  itemImage: {
-    minHeight: 100,
+  gridTile: {
+    height: '100% !important',
   },
 }));
 
-const GlobalMarketEntry = () => {
+const GlobalMarketEntry = ({ entry }) => {
   const classes = useStyles();
+
+  const materials = entry.Materials;
+
+  const renderItems = () => {
+    let items = [];
+    for (let i = 0; i < 5; i++) {
+      if (!materials[i]) {
+        items.push(
+          <GridListTile key={i} cols={1} className={classes.gridTile}>
+            <Paper className={classes.paper} elevation={3} square>
+              <img
+                className={classes.image}
+                src="/assets/NoContent.png"
+                alt="No Content"
+              />
+              N/A
+            </Paper>
+          </GridListTile>
+        );
+      } else {
+        const imageLink = '/assets/' + materials[i].Material.Picture + '.png';
+        const quantity = materials[i].Quantity;
+        items.push(
+          <GridListTile key={i} cols={1} className={classes.gridTile}>
+            <Paper className={classes.paper} elevation={3} square>
+              <img
+                className={classes.image}
+                src={imageLink}
+                alt={materials[i].Material.Picture}
+              />
+              {quantity}
+            </Paper>
+          </GridListTile>
+        );
+      }
+    }
+    return items;
+  };
 
   return (
     <React.Fragment>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          User Name
+          User: {entry.User.Username}
         </Grid>
         <Grid>
-          <GridList className={classes.gridList} cols={5} cellHeight={100}>
-            <GridListTile cols={1} className={classes.gridList}>
-              <Paper className={classes.paper} elevation={3} square>
-                item
-              </Paper>
-            </GridListTile>
-            <GridListTile>
-              <Paper className={classes.paper} elevation={3} square>
-                item
-              </Paper>
-            </GridListTile>
-            <GridListTile>
-              <Paper className={classes.paper} elevation={3} square>
-                item
-              </Paper>
-            </GridListTile>
-            <GridListTile>
-              <Paper className={classes.paper} elevation={3} square>
-                item
-              </Paper>
-            </GridListTile>
-            <GridListTile>
-              <Paper className={classes.paper} elevation={3} square>
-                item
-              </Paper>
-            </GridListTile>
+          <GridList cols={5} cellHeight={100}>
+            {renderItems()}
           </GridList>
         </Grid>
         <Grid item xs={12}>
-          Price
+          Price: {entry.SuggestedPrice}
         </Grid>
       </Grid>
     </React.Fragment>
