0c9bd57cb43fbf52b7c6d3972e9150c4700b1d39
ImBluue <baudvalentin74@gmail.com>
2020-05-04 18:25:16 +0200

    Fix react CI by re-adding app.test.js

--- client/src/components/Copyright.js
+++ client/src/components/Copyright.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import { Typography } from '@material-ui/core';
-import MaterialLink from '@material-ui/core/Link';
+import { Typography } from "@material-ui/core";
+import MaterialLink from "@material-ui/core/Link";
 
 const Copyright = () => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <MaterialLink color="inherit" href="#">
         SSSF Project
-      </MaterialLink>{' '}
+      </MaterialLink>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </Typography>
   );
 };
